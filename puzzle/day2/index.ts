b14ac24c5eb0f42e4f3771b1b5c5efc0947afd17
NiNoX14 <hardstuckd5lul@gmail.com>
2024-12-02 21:29:03 +0200

    feat: "solve" day 2 part 2

--- puzzle/day2/index.ts
+++ puzzle/day2/index.ts
@@ -1,36 +1,60 @@
 import { readInput } from '../../lib';
 
-const pareseInput = (input: string) => {
+const parseInput = (input: string) => {
   return input.split('\n').map((row) => row.split(' ').map((a) => +a));
 };
 
-const solve = (reports: number[][]) => {
+const checkReport = (report: number[]) => {
+  const isIncreasing = report[0] < report[report.length - 1];
+  for (let i = 0; i < report.length - 1; i++) {
+    const [curr, next] = [report[i], report[i + 1]];
+
+    const diff = Math.abs(curr - next);
+    if (diff > 3 || curr === next) {
+      return i;
+    }
+    if (curr > next && isIncreasing) {
+      return i;
+    }
+    if (curr < next && !isIncreasing) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const solve1 = (reports: number[][]) => {
   let safeReports = 0;
   reports.forEach((report) => {
-    let isIncreasing = report[report.length - 1] < report[report.length - 2];
-    let isSafe = true;
-    for (let i = report.length; i >= 0; i--) {
-      const [curr, next] = [report[i], report[i - 1]];
-      const diff = Math.abs(curr - next);
-      if (diff > 3 || curr === next) {
-        isSafe = false;
-        break;
-      }
-
-      if (curr > next && isIncreasing) {
-        isSafe = false;
-        break;
-      }
-      if (curr < next && !isIncreasing) {
-        isSafe = false;
-        break;
-      }
-    }
-    if (isSafe) {
+    if (checkReport(report) === -1) {
       safeReports++;
     }
   });
   return safeReports;
 };
 
-console.log(solve(pareseInput(await readInput(import.meta.dir))));
+const solve2 = (reports: number[][]) => {
+  let safeReports = 0;
+  const corr: number[][] = [];
+  reports.forEach((report) => {
+    let index = checkReport(report);
+    if (index >= 0) {
+      for (let k = 0; k < report.length; k++) {
+        const copy = [...report];
+        copy.splice(k, 1);
+        index = checkReport(copy);
+        if (index == -1) {
+          // console.log('dampener',k,  l1, safe)
+          break;
+        }
+      }
+    }
+    if (index < 0) {
+      safeReports++;
+      corr.push(report);
+    }
+  });
+  return [safeReports, corr] as const;
+};
+
+console.log(solve2(parseInput(await readInput(import.meta.dir))));
